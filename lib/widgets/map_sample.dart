97674f8b43ed59a6a69d73525e253a3f1805d2b4
youssef-hadwa <yousseftube126@gmail.com>
2025-03-22 03:49:28 +0200

    add custom json style for google map  and preview it

---
+++ lib/widgets/map_sample.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
+  @override
+  State<MapSample> createState() => _MapSampleState();
+}
+
+class _MapSampleState extends State<MapSample> {
+  late CameraPosition initialCameraPosition;
+  late GoogleMapController mapController;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+        target: LatLng(30.78632846612454, 30.999528414111044), zoom: 13);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        GoogleMap(
+          cameraTargetBounds: CameraTargetBounds(
+            LatLngBounds(
+                southwest: LatLng(30.57453740183841, 30.71606382514833),
+                northeast: LatLng(30.96953570951455, 31.166773656135007)),
+          ),
+          onMapCreated: (controller) {
+            mapController = controller;
+            initMapStyle();
+          },
+          initialCameraPosition: initialCameraPosition,
+        ),
+        Positioned(
+            bottom: 12,
+            right: 12,
+            left: 12,
+            child: ElevatedButton(
+                onPressed: () {
+                  mapController.animateCamera(CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                          target:
+                              LatLng(30.798860738915106, 30.957435117150393),
+                          zoom: 17)));
+                },
+                child: const Text('change camera')))
+      ]),
+    );
+  }
+
+  initMapStyle() async {
+    var darkMapStyle = await DefaultAssetBundle.of(context)
+        .loadString('assets/google_maps_styles/google_map_dark_style.json');
+
+    mapController.setMapStyle(darkMapStyle);
+  }
+}
+
+//world view 0 -> 3
+//country view 4 -> 6
+// city view 10-> 12
+//street view 13 -> 17
+//building view 18 -> 20
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:google_map/main.dart';
+import 'package:google_map/widgets/map_sample.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
